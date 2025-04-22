72b8641b763a32f0180efd7e01232043c3147716
BYADIR-IBRAHIM-A <BYADER144110@GMAil.com>
2025-04-22 12:03:56 +0300

    add code

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Products.Models;
+
+namespace Products.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public IActionResult Index()
+        {
+            var products = new List<ProductModel>
+            {
+                new ProductModel { Id = 1, Name = "هور الفلوكس الوردي", Price = 22, ImagePath = "flower1.jpg" },
+                new ProductModel { Id = 2, Name = "زهرة الجوري", Price = 19, ImagePath = "flower2.jpg" },
+                new ProductModel { Id = 3, Name = "ورود بيضاء", Price = 20, ImagePath = "flower3.jpg" }
+            };
+            return View(products);
+        }
+
+        public IActionResult Details(int id)
+        {
+            var product = new ProductModel { Id = id, Name = "مشروب ماتشا لاتيه", Price = 22 };
+            return View(product);
+        }
+
+        public IActionResult Random()
+        {
+            var product = new ProductModel { Id = 1, Name = "مشروب ماتشا لاتيه", Price = 19 };
+            return View(product);
+        }
+    }
+}
---
+++ Models/ProductModel.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Products.Models
+{
+    //تعرف مجموعة List من نوع ProductModel داخل الدالة Index.
+    public class ProductModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public float Price { get; set; }
+        public string ImagePath { get; set; } = string.Empty; // مسار الصورة
+    }
+    /*
+    public class ProductDetailsModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public float Price { get; set; }
+        public string Description { get; set; } = string.Empty;
+    }
+    */
+}
